1f8d88bc5a04a4d47bca0c6fa7d5897a790bb6ae
EDM2001 <michaelarodriguez@mavs.uta.edu>
2015-11-08 21:58:18 -0600

    Textures

--- app/src/main/java/com/example/insy4308/opengl/MainActivity.java
+++ app/src/main/java/com/example/insy4308/opengl/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.insy4308.opengl;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
 import android.os.Bundle;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -10,9 +14,10 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends Activity implements  GLSurfaceView.Renderer {
 
-
+    Context context;
     float animation = 0.0f;
     Sprite sprite = null;
+    final int[] textureHandle = new int[1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +30,24 @@ public class MainActivity extends Activity implements  GLSurfaceView.Renderer {
         setContentView(glView);
 
 
-        sprite = new Sprite();
+        sprite = new Sprite(context);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config)
     {
         GLES20.glClearColor(0.0f, 0.3921568627450980f, 0.6941176470588235f, 0.0f);
+
+
+        GLES20.glGenTextures(1, textureHandle, 0);
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.sunrays_background, options);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0]);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+        bitmap.recycle();
     }
 
     @Override
@@ -45,14 +61,14 @@ public class MainActivity extends Activity implements  GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl)
     {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        animation += 1.0f;
+        sprite.draw(textureHandle[0]);
+        if(animation<360.0f)
+            sprite.setRotation(animation);
 
-        animation += 0.01f;
 
-        sprite.setRotation(animation*100);
-        sprite.setHeight(0.2f);
-        sprite.setWidth(0.2f);
 
-        sprite.draw();
+
     }
 
 
--- app/src/main/java/com/example/insy4308/opengl/Sprite.java
+++ app/src/main/java/com/example/insy4308/opengl/Sprite.java
@@ -1,6 +1,10 @@
 package com.example.insy4308.opengl;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
 import android.opengl.Matrix;
 
 import java.nio.ByteBuffer;
@@ -8,13 +12,16 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 public class Sprite {
+    Context context;
     static int program = -1;
     static FloatBuffer geometryBuffer = null;
+    static FloatBuffer textureBuffer = null;
     float centerX = 0.0f;
     float centerY = 0.0f;
     float width = 1.0f;
     float height = 1.0f;
     float rotation = 0.0f;
+
 
     public float getCenterX()
     {
@@ -57,26 +64,41 @@ public class Sprite {
         this.rotation = rotation;
     }
 
+    public Sprite(Context context)
+    {
+        this.context = context;
+    }
+
     private void initialize() {
         String vertexShaderSource = "" +
                 "uniform mat4 modelView;" +
                 "attribute vec3 position;" +
+                "attribute vec2 atexCord" +
+                "varying vec2 vtexCord" +
                 "" +
                 "void main()" +
                 "" +
                 "{" +
+                "" +//passes the atexCord to fragment shader
+                "" +//you'll have to link them below
+                "vtexCord = atexCord;" +
                 "" +
                 "   gl_Position = modelView * vec4(position, 1.0);" +
                 "" +
                 "}";
 
-        String fragmentShaderSource = "" +
+        String fragmentShaderSource = "precision mediump float" +
+                "uniform sampler2D utexture" +
+                "varying vec2 vtexCord" +
                 "void main()" +
                 "" +
                 "{" +
                 "" +
-                "   gl_FragColor = vec4(0.96, 0.5, 0.14, 1.0);" +
+                "   gl_FragColor = texture2D(utexture, vtexCord);" +
                 "}";
+
+
+
 
         int vertextShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
         GLES20.glShaderSource(vertextShader, vertexShaderSource);
@@ -92,7 +114,7 @@ public class Sprite {
         GLES20.glAttachShader(program, vertextShader);
         GLES20.glAttachShader(program, fragmentShader);
         GLES20.glBindAttribLocation(program, 0, "position");
-        GLES20.glBindAttribLocation(program, 1, "texture");
+        GLES20.glBindAttribLocation(program, 1, "atextCord");
         GLES20.glLinkProgram(program);
         String programLinkLog = GLES20.glGetProgramInfoLog(program);
 
@@ -100,10 +122,18 @@ public class Sprite {
 
         float[] geometry =
                 {
-                        -0.5f, -0.5f, 0.0f,
-                        0.5f, -0.5f, 0.0f,
-                        -0.5f, 0.5f, 0.0f,
-                        0.5f, 0.5f, 0.0f,
+                        -1.5f, -1.5f, 0.0f,
+                        1.5f, -1.5f, 0.0f,
+                        -1.5f, 1.5f, 0.0f,
+                        1.5f, 1.5f, 0.0f,
+                };
+
+        float[] texture =
+                {
+                        -1.5f, -1.5f,
+                        1.5f, -1.5f,
+                        -1.5f, 1.5f,
+                        1.5f, 1.5f
                 };
 
         ByteBuffer geometryByteBuffer = ByteBuffer.allocateDirect(geometry.length * 4);
@@ -111,24 +141,37 @@ public class Sprite {
         geometryBuffer = geometryByteBuffer.asFloatBuffer();
         geometryBuffer.put(geometry);
         geometryBuffer.rewind();
+
+        ByteBuffer textureByteBuffer = ByteBuffer.allocateDirect(texture.length * 4);
+        textureByteBuffer.order(ByteOrder.nativeOrder());
+        textureBuffer = textureByteBuffer.asFloatBuffer();
+        textureBuffer.put(texture);
     }
 
-    public void draw()
+    public void draw(int textureHandle)
     {
         if (program < 0)
             initialize();
 
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle);
+        GLES20.glUniform1i(GLES20.glGetUniformLocation(program, "utexture"), 0);
+        GLES20.glVertexAttribPointer(GLES20.glGetAttribLocation(program, "atextCord"), 2, GLES20.GL_FLOAT, false, 0, 0);
+        GLES20.glEnableVertexAttribArray(GLES20.glGetAttribLocation(program, "atextCord"));
+
         float[] modelView = new float[16];
-        Matrix.setIdentityM(modelView,0);
+        Matrix.setIdentityM(modelView, 0);
         Matrix.translateM(modelView, 0, centerX, centerY, 0.0f);
         Matrix.rotateM(modelView, 0, rotation, 0.0f, 0.0f, 1.0f);
         Matrix.scaleM(modelView, 0, width, height, 1.0f);
         GLES20.glUniformMatrix4fv(GLES20.glGetUniformLocation(program, "modelView"), 1, false, modelView, 0);
 
+
         int elements = 3;
         GLES20.glVertexAttribPointer(0, elements, GLES20.GL_FLOAT, false, 0, geometryBuffer);
         GLES20.glEnableVertexAttribArray(0);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, geometryBuffer.capacity() / elements);
+
 
     }
 
